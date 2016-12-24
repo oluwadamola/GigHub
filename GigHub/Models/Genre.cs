00bf23853c1e5bf00f05001f47a145fa65b044b9
oluwadamola.stephen <stephenajoses@gmail.com>
2016-12-23 23:16:03 -0800

    initialcommit

---
+++ GigHub/Models/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.Models
+{
+    public class Genre
+    {
+        public  byte GenreId { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string GenreName { get; set; }          
+    }
+}
