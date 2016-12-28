0a833736b21aa2dbff33832a94884c1584c932cf
oluwadamola.stephen <stephenajoses@gmail.com>
2016-12-27 18:03:47 -0800

    Styling the index page

--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -18,6 +18,7 @@ namespace GigHub.Controllers
         {
             var upcommingGigs = _dbContext.Gigs
                 .Include(g => g.Artist)
+                .Include(g => g.Genre)
                 .Where(g => g.DateTime > DateTime.Now);
             return View(upcommingGigs);
         }
