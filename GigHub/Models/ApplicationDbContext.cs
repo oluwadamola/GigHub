209e3053378349e9f2a2fd0ede1d34f28aa80e56
oluwadamola.stephen <stephenajoses@gmail.com>
2016-12-28 21:26:46 -0800

    .Net WebApi

--- GigHub/Models/ApplicationDbContext.cs
+++ GigHub/Models/ApplicationDbContext.cs
@@ -1,5 +1,5 @@
-﻿using System.Data.Entity;
-using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 
 namespace GigHub.Models
 {
@@ -12,9 +12,18 @@ namespace GigHub.Models
 
         public DbSet<Gig> Gigs { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Attendance> Attendances{get; set;  }
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Gig).WithMany()
+                .WillCascadeOnDelete(false);
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
